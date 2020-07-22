8b5d1d9261e0218a7fb1032203d2a6088a8e4d19
Toastacular <68423986+Toastacular@users.noreply.github.com>
2020-07-22 21:44:56 +0100

    blo

--- Javascripts/Source/Places/place007.js
+++
@@ -1,6 +0,0 @@
-// Library Study Area
-
-function ShowPlace7()
-{
-	showGeneralPlace('Study Area', 'library-studyarea.jpg', 'The study area for the library.', "go to the library reception", 3);
-}
--- Javascripts/Source/Places/place500.js
+++
@@ -1,6 +0,0 @@
-// Elian's Lair
-
-function ShowPlace500()
-{
-	showGeneralPlace('Elian\'s Lair', 'apartment3.jpg', 'Elian\'s lair');
-}
